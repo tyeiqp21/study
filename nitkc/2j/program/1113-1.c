d779bb6ed5783971ccdee6b3ab978dd4eecb8aff
suracuto <suracuto@users.noreply.github.com>
2015-11-17 12:57:31 +0900

    First commit

---
+++ nitkc/2j/program/1113-1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void){
+  int i;
+  char *buff;
+
+  if((buff = (char*)malloc(sizeof(char) * 1024)) == NULL){
+    fprintf( stderr, "Failed to malloc.\n");
+    exit(1);
+  }
+
+  srand((unsigned int)time(NULL));
+
+  fgets(buff, sizeof(buff), stdin);
+  sscanf(buff, "%d", &i);
+
+  if(i <= 0){
+    fprintf(stderr, "HA???Kireso-----\n");
+  }
+
+  while(0 < i){
+    printf("%c", rand()%26 + 'a');
+    --i;
+  }
+  printf("\n");
+
+  free(buff);
+  return(0);
+}
